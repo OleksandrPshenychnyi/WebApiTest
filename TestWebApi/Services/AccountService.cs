45e10d37da6fe14d5fb8e9aeca5df4c6b188fb46
OleksandrPshenychnyi <73752533+OleksandrPshenychnyi@users.noreply.github.com>
2022-01-10 10:55:00 +0200

    services add

---
+++ TestWebApi/Services/AccountService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using System;
+using System.Threading.Tasks;
+using TestWebApi.Interfaces;
+using TestWebApi.Models;
+using TestWebApi.ModelsDTO;
+
+namespace TestWebApi.Services
+{
+    public class AccountService : IAccountService
+    {
+
+        readonly ProgContext db;
+        readonly IMapper _mapper;
+        public AccountService(ProgContext progContext, IMapper mapper)
+        {
+            db = progContext;
+            _mapper = mapper;
+        }
+        public async Task<Account> GetAccountForContact(ContactLinkToAccountDTO contactDTO)
+        {
+            var account = await db.Accounts.FindAsync(contactDTO.AccountName);
+            return account;
+        }
+        public async Task<Account> AccountCreationAsync(AccountCreationDTO accountDTO)
+        {
+            var mappedAccount = _mapper.Map<Account>(accountDTO);
+            //Account account = new Account()
+            //{
+            //    AccountName = accountDTO.AccountName
+
+            //};
+            try
+            {
+                db.Accounts.Add(mappedAccount);
+                // await db.SaveChangesAsync(); 
+                //List<Contact> contacts = new List<Contact>();
+                //foreach (var contact in accountDTO.Contacts)
+                //{
+                //    contacts.Add(new Contact()
+                //    {
+                //        Email = contact.Email,
+                //        FirstName = contact.FirstName,
+                //        LastName = contact.LastName,
+                //        Account = mappedAccount
+                //    });
+                //}
+                db.Contacts.AddRange(mappedAccount.Contacts);
+                await db.SaveChangesAsync();
+                return mappedAccount;
+            }
+            catch
+            {
+                throw new Exception("Account name and contact emails must be unique!");
+            }
+        }
+        public async Task<Account> GetAccountForIncident(IncidentCreationDTO incidentDTO)
+        {
+            var account = await db.Accounts.FindAsync(incidentDTO.AccountName);
+            return account;
+        }
+    }
+}
---
+++ TestWebApi/Services/ContactService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TestWebApi.Interfaces;
+using TestWebApi.Models;
+using TestWebApi.ModelsDTO;
+
+namespace TestWebApi.Services
+{
+    public class ContactService : IContactService
+    {
+        readonly ProgContext db;
+        readonly IMapper _mapper;
+        public ContactService(ProgContext progContext, IMapper mapper)
+        {
+            db = progContext;
+            _mapper = mapper;
+        }
+        public async Task<Contact> ContactCreationAsync(ContactLinkToAccountDTO contactDTO, Account account )
+        {
+            
+            var mappedContact = _mapper.Map<Contact>(contactDTO);
+            mappedContact.Account = account;
+            db.Contacts.Add(mappedContact);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new("Email is already exist!", ex);
+
+            }
+            return mappedContact;
+        }
+
+        public async Task ContactCreationForIncidentAsync(IncidentCreationDTO incidentDTO, Account account)
+        {
+            var contact = await db.Contacts.FindAsync(incidentDTO.ContactEmail);
+            if (contact != null)
+            {
+                contact.FirstName = incidentDTO.ContactFirstName;
+                contact.LastName = incidentDTO.ContactLastName;
+                contact.Account = account;
+                db.Contacts.Update(contact);
+            }
+            else
+            {
+                Contact contactToCreate = new Contact()
+                {
+                    FirstName = incidentDTO.ContactFirstName,
+                    LastName = incidentDTO.ContactLastName,
+                    Email = incidentDTO.ContactEmail,
+                    Account = account
+                };
+                db.Contacts.Add(contactToCreate);
+            }
+            await db.SaveChangesAsync();
+        }
+    }
+}
